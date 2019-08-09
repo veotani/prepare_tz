39b0e778dc804e25e5a2a17f4c0d6d091f60c6be
Anton Orlov <ion6431@gmail.com>
2019-08-09 12:36:33 +0300

    renaming + algorithm fix

--- counters/SimpleCounter.py
+++ counters/SimpleCounter.py
@@ -9,31 +9,30 @@ class SimpleCounter:
     def __init__(self):
         pass
 
-    def getCourseModel(self, cost, author_name, course_name):
-        rounded_cost = round(cost, 2)
+    def get_course_model(self, course_info):
+        rounded_cost = Decimal(str(round(course_info.cost, 2)))
 
-        lectures_cost  = 80 * rounded_cost // 100
-        structure_cost = 5 * rounded_cost // 100
-        guidance_cost  = 5 * rounded_cost // 100
-        tests_cost     = Decimal(rounded_cost) - (Decimal(lectures_cost) + Decimal(structure_cost) + Decimal(guidance_cost))
+        lectures_cost  = 80 * (rounded_cost // 100)
+        structure_cost = 5 *  (rounded_cost // 100)
+        guidance_cost  = 5 *  (rounded_cost // 100)
+        tests_cost     = rounded_cost - (lectures_cost + structure_cost + guidance_cost)
 
-        lectures_price  = 800
-        structure_price = 100
-        guidance_price  = 100
-        tests_price     = 0
+        lectures_price  = Decimal('800')
+        structure_price = Decimal('100')
+        guidance_price  = Decimal('100')
+        tests_hours     = Decimal('0')
 
-        for i in range(100, 500):
+        for i in range(1, 50):
             if int(tests_cost * 100) % i == 0:
-                tests_price = i
-                break
+                tests_hours = i
         
-        if tests_price == 0:
-            raise Exception('There is no divisor for tests cost.')
+        if tests_hours == 0:
+            raise Exception(f'There is no divisor for tests cost. Cost is {tests_cost}.')
         
         lectures_hours  = lectures_cost / lectures_price
         structure_hours = structure_cost / structure_price
         guidance_hours  = guidance_cost / guidance_price
-        tests_hours     = tests_cost / tests_price
+        tests_price     = tests_cost / tests_hours
 
         course_prices = CoursePrices(
             lectures_price, lectures_hours, lectures_cost, 
@@ -41,7 +40,7 @@ class SimpleCounter:
             tests_price, tests_cost, tests_hours,
             guidance_price, guidance_cost, guidance_hours,
             rounded_cost,
-            author_name, course_name
+            course_info.author_name, course_info.course_name
         )
 
         return course_prices
