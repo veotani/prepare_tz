f8473b3031d1c2e6f3e6d07d7a56a1e5264537a7
Anton Orlov <ion6431@gmail.com>
2019-08-12 16:37:54 +0300

    it's more data required now

--- models/CourseInfo.py
+++ models/CourseInfo.py
@@ -5,7 +5,74 @@ This is the information that input files must contain.
 '''
 
 class CourseInfo:
-    def __init__(self, course_name, author_name, cost):
+    def __init__(self, course_name, author_name, cost,
+        passport_seria_number,
+        passport_created_location,
+        passport_created_date,
+        ITN,
+        INILA,
+        tax_start_date,
+        position,
+        bank_requisites,
+        degree,
+        itn_1,
+        itn_2,
+        itn_3,
+        itn_4,
+        itn_5,
+        itn_6,
+        itn_7,
+        itn_8,
+        itn_9,
+        itn_10,
+        itn_11,
+        itn_12,
+        inila_1,
+        inila_2,
+        inila_3,
+        inila_4,
+        inila_5,
+        inila_6,
+        inila_7,
+        inila_8,
+        inila_9,
+        inila_10,
+        inila_11
+    ):
         self.course_name = course_name
         self.author_name = author_name
         self.cost        = cost
+        self.reward      = reward
+        self.birthdate   = birthdate
+        self.passport_seria_number = passport_seria_number
+        self.passport_created_location = passport_created_location
+        self.passport_created_date = passport_created_date
+        self.ITN = ITN
+        self.INILA = INILA
+        self.tax_start_date = tax_start_date
+        self.position = position
+        self.bank_requisites = bank_requisites
+        self.degree = degree
+        self.itn_1 = itn_1
+        self.itn_2 = itn_2
+        self.itn_3 = itn_3
+        self.itn_4 = itn_4
+        self.itn_5 = itn_5
+        self.itn_6 = itn_6
+        self.itn_7 = itn_7
+        self.itn_8 = itn_8
+        self.itn_9 = itn_9
+        self.itn_10 = itn_10
+        self.itn_11 = itn_11
+        self.itn_12 = itn_12
+        self.inila_1 = inila_1
+        self.inila_2 = inila_2
+        self.inila_3 = inila_3
+        self.inila_4 = inila_4
+        self.inila_5 = inila_5
+        self.inila_6 = inila_6
+        self.inila_7 = inila_7
+        self.inila_8 = inila_8
+        self.inila_9 = inila_9
+        self.inila_10 = inila_10
+        self.inila_11 = inila_11
