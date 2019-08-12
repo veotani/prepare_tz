575790bda62f535c77cd77f724bb918bdb7bb316
Anton Orlov <ion6431@gmail.com>
2019-08-12 17:21:28 +0300

    evaluate digits inside model while initialisation

--- models/CourseInfo.py
+++ models/CourseInfo.py
@@ -14,30 +14,7 @@ class CourseInfo:
         tax_start_date,
         position,
         bank_requisites,
-        degree,
-        itn_1,
-        itn_2,
-        itn_3,
-        itn_4,
-        itn_5,
-        itn_6,
-        itn_7,
-        itn_8,
-        itn_9,
-        itn_10,
-        itn_11,
-        itn_12,
-        inila_1,
-        inila_2,
-        inila_3,
-        inila_4,
-        inila_5,
-        inila_6,
-        inila_7,
-        inila_8,
-        inila_9,
-        inila_10,
-        inila_11
+        degree
     ):
         self.course_name = course_name
         self.author_name = author_name
@@ -53,26 +30,27 @@ class CourseInfo:
         self.position = position
         self.bank_requisites = bank_requisites
         self.degree = degree
-        self.itn_1 = itn_1
-        self.itn_2 = itn_2
-        self.itn_3 = itn_3
-        self.itn_4 = itn_4
-        self.itn_5 = itn_5
-        self.itn_6 = itn_6
-        self.itn_7 = itn_7
-        self.itn_8 = itn_8
-        self.itn_9 = itn_9
-        self.itn_10 = itn_10
-        self.itn_11 = itn_11
-        self.itn_12 = itn_12
-        self.inila_1 = inila_1
-        self.inila_2 = inila_2
-        self.inila_3 = inila_3
-        self.inila_4 = inila_4
-        self.inila_5 = inila_5
-        self.inila_6 = inila_6
-        self.inila_7 = inila_7
-        self.inila_8 = inila_8
-        self.inila_9 = inila_9
-        self.inila_10 = inila_10
-        self.inila_11 = inila_11
+            
+        self.itn_1 = self.ITN[0]
+        self.itn_2 = self.ITN[1]
+        self.itn_3 = self.ITN[2]
+        self.itn_4 = self.ITN[3]
+        self.itn_5 = self.ITN[4]
+        self.itn_6 = self.ITN[5]
+        self.itn_7 = self.ITN[6]
+        self.itn_8 = self.ITN[7]
+        self.itn_9 = self.ITN[8]
+        self.itn_10 = self.ITN[9]
+        self.itn_11 = self.ITN[10]
+        self.itn_12 = self.ITN[11]
+        self.inila_1 = self.INILA[0]
+        self.inila_2 = self.INILA[1]
+        self.inila_3 = self.INILA[2]
+        self.inila_4 = self.INILA[3]
+        self.inila_5 = self.INILA[4]
+        self.inila_6 = self.INILA[5]
+        self.inila_7 = self.INILA[6]
+        self.inila_8 = self.INILA[7]
+        self.inila_9 = self.INILA[8]
+        self.inila_10 = self.INILA[9]
+        self.inila_11 = self.INILA[10]
